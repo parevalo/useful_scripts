3636fc784776222d909ab344da11133644039826
parevalo <parevalo@bu.edu>
2016-02-18 18:21:56 -0500

    Create script to run multi_gen_date

--- run_multi_gen_date.sh
+++ run_multi_gen_date.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
+#$ -V
+# -j y
 
 # Executes gen_date_file.sh and places the output in the corresponding image folder.
-# It is easier to keep the filde in the image folder because it can be used
-# by all of the models (e.g. FIT1, etc) without having to duplicate the file.
+# It is easier to keep the output file in the image folder because it can be used
+# by all of the models (e.g. FIT1, etc) without having to duplicate it.
 
-scn_path=/projectnb/landsat/projects/Colombia/images/
+scn_path=/projectnb/landsat/projects/Colombia/images
 script=/projectnb/landsat/users/parevalo/yatsm/scripts/gen_date_file.sh
 
 for DIR in $scn_path/*/ ; do
 	cd ${DIR}
-	path=${DIR:4:1}
-	row=${DIR:6:2}
-	$script /images $path$row_input.csv
+	pr=$(echo ${DIR} | awk -F'/' '{ print $7 }') # Get folder name with P-R
+	path=${pr:2:1}
+	row=${pr:4:2}
+	output=$path$row"_input.csv"
+	if [ -f $output ]; then
+		echo "File already exists in ${DIR}"
+
+	else
+		echo "Creating file in ${DIR}"
+		script /images $output
+	fi	
 done
 
 
