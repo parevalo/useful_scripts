0dfe215943fa1ddbeece8b5068ee0135d5da8baa
parevalo <parevalo@bu.edu>
2016-02-18 12:51:39 -0500

    More generalized version of createfolders

--- createfolders.sh
+++ createfolders.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then
+    echo "Error - please specify the image directory that stores all the scenes
+	and the name of the subfolder (and nested subfolders) to create in each of them. Usage:"
+    echo "    $0 <directory> <subfolder>"
+    exit 1
+fi
+
 for DIR in $1/*/ ; do
-    if [ -d ${DIR}img_old ]; then
+    if [ -d ${DIR}$2 ]; then
         echo "Folder already exists in ${DIR}"
 
     else
-        mkdir "${DIR}img_old"
+        mkdir "${DIR}$2"
         echo "Folder created in ${DIR}"
+		 
     fi
 done
-
