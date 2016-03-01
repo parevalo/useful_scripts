3cf7bee3e3f7522f898362759aabfe89083e1674
parevalo <parevalo@bu.edu>
2016-02-29 20:56:13 -0500

    Short script to find example image

---
+++ find_example_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script to find the first Landsat 7 folder in an image folder
+# This is used to replace the example image in the scripts for each scene
+
+img=$(find . -maxdepth 1 -type d -name "*LE7*" | head -1 )
+# An alternative would be:
+#img=$(find . -maxdepth 1 -type d -regextype posix-extended -regex "./LE7.*" | head -1 )
+echo $(basename $img)
+echo $(basename $img)_stack
