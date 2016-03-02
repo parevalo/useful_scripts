4383523b04efc4223a69805130e42428caf6aea1
parevalo <parevalo@bu.edu>
2016-03-02 14:48:43 -0500

    Prevent copy from overwritting files

--- copy_files.sh
+++ copy_files.sh
@@ -3,10 +3,9 @@
 #$ -j y
 
 # This is a generic script to copy files or folders into any specific
-# FIT subfolder inside all of the scene folders. This script is intended for use
-# in BRAND NEW folders, as it has the potential to overwrite scripts available
-# in current FIT folders. The goal is to use it when brand new models are run
-# for the entire study area.
+# FIT subfolder inside all of the scene folders. The copy function won't
+# overwrite any existing file.
+#
 
 if [ $# -lt 2 ]; then
 	echo "Error - please specify the files/folders to copy and the desired
@@ -18,11 +17,11 @@ fi
 parent="/usr3/graduate/parevalo/test"
 current="/projectnb/landsat/projects/Colombia/workflow"
 
-# Copy if file is newer or inexistent, be verbose, recursive
+# Do not overwrite, be verbose, recursive
 for DIR in $parent/*/ ; do
 	cd ${DIR}
 	if [ -d ${DIR}$2 ]; then
-		cp -u -v -r $current/$1 $2
+		cp -n -v -r $current/$1 $2
 		echo "Files created in ${DIR}$2"
 	else
 		echo "$current$1"
