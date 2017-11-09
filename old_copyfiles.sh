6f6fd96880c54fa69e0fc4005147ba05d33e3d5f
parevalo <parevalo@bu.edu>
2017-11-09 15:44:44 -0500

    Reorganize repo

--- old_copyfiles.sh
+++
@@ -1,9 +0,0 @@
-cd /projectnb/landsat/projects/Colombia/images/008060/Results/FIT1
-path=/projectnb/landsat/projects/Colombia/images/
-
-for i in $(seq -w 006 008); do
-	for j in $(seq -w 059 060); do
-		cp Multimap.sh $path$i$j/Results/FIT1
-	done
-done
-
