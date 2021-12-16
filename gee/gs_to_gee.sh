81c90fda846b393888296205ecdddeb858276d60
parevalo <parevalo@bu.edu>
2021-12-16 14:47:27 -0600

    updated scripts

--- gee/gs_to_gee.sh
+++ gee/gs_to_gee.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-folder="/mnt/c/Users/paulo/Downloads"
+folder="/mnt/c/Users/paulo/Downloads/mlongo_gbc_2016_maps"
 cd $folder
 
-for f in $(find ./ -maxdepth 1 -name "*.csv"); do 
-	fname=$(basename "$f" .csv)
+for f in $(find ./ -maxdepth 1 -name "*.tif"); do 
+	fname=$(basename "$f" .tif)
 	echo "Upload job for $fname"
-	earthengine upload table \
-	 --asset_id=users/parevalo_bu/biomass/glas/$fname \
-	 gs://gee_biomass/$fname".csv" 
+	earthengine upload image \
+	 --asset_id=users/parevalo_bu/biomass/longo_2016/$fname \
+	 gs://gee_biomass/$fname".tif" 
 done 
 
